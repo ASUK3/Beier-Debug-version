698f6f4f6859e1e733ae31def916e2bc45b89eea
ASUK3 <120798558+ASUK3@users.noreply.github.com>
2023-04-29 16:58:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
-rm -rf ./package/base-files/files/etc/banner
-rm -rf ./feeds/luci/themes/luci-theme-argon
-rm -rf ./feeds/haibo/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/luci-theme-argon
