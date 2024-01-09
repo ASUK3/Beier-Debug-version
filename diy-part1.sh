72b139994768e98fe5645e8117e38013261b90a9
ASUK3 <120798558+ASUK3@users.noreply.github.com>
2024-01-10 02:22:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,10 +25,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Ma
 #定时任务软件源
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 #mosdns软件源
-rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
